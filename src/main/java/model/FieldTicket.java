71187c78c747753ae1fcc01a2a4b42f52c17524d
Kopranych <kopranych@gmail.com>
2018-07-28 22:38:00 +0300

    Добавил классы FieldTicket и TicketString

---
+++ src/main/java/model/FieldTicket.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FieldTicket {
+    private Set<Column> setColumn = new HashSet<Column>();
+    private Set<TicketString> setTicketString = new HashSet<TicketString>();
+}
