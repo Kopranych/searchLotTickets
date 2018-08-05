71f3182765dc6ef774315718ca492eb7070680ac
Kopranych <kopranych@gmail.com>
2018-08-05 13:21:24 +0300

    [UPDATE] - добавил метод verificationNextTour(Ticket)

--- src/main/java/lottery/verification/VerificationWinningTickets.java
+++ src/main/java/lottery/verification/VerificationWinningTickets.java
@@ -5,14 +5,13 @@ import model.Ticket;
 import model.TicketRow;
 import ru.bpirate.vsrftools.Tools;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class VerificationWinningTickets {
     public static List<Integer> countNumber = new LinkedList<>();
 
-    public static void reciveNumbers(int number) {
-    }
 
     public static void verificationWinningTicket(List<Ticket> ticketList, int number) {
         countNumber.add(number);
@@ -23,12 +22,21 @@ public class VerificationWinningTickets {
             for (Ticket ticket : ticketList) {
                 verificationFirstTour(ticket);
             }
-        }
-        if (countNumber.size() >= 15 || countNumber.size()<=29) {
+        }else if (countNumber.size() >= 15 || countNumber.size()<=29) {
             for (Ticket ticket : ticketList) {
                 verificationSecondTour(ticket);
             }
+        }else if(countNumber.size()>=30){
+            Iterator<Ticket> itr = ticketList.iterator();
+            while(itr.hasNext()){
+                Ticket ticket = itr.next();
+                boolean isWinner = verificationNextTour(ticket);
+                if(isWinner){
+                    itr.remove();
+                }
+            }
         }
+
 
 
     }
@@ -108,8 +116,37 @@ public class VerificationWinningTickets {
                     "Билет номер " + ticket.getNumber() + "ВЫИГРАЛ ДЖЕКПОТ!!!!!!!! СОВПАДЕНИЕМ 15 ЧИСЕЛ");
             return;
         }
+    }
 
+    public static boolean verificationNextTour(Ticket ticket) {
+        //третий и последующий тур
+        boolean isWinner = true;
+        int countCrossedNumber = 0;
 
+        for (TicketRow ticketRow : ticket.getTopField().getSetTicketRow()) {
+            for (Cell cell : ticketRow.getSetCell()) {
+                if (!cell.isCrossed()) {
+                    isWinner = false;
+                    return isWinner;
+                }
+                countCrossedNumber++;
+            }
+        }
+        for (TicketRow ticketRow : ticket.getBotField().getSetTicketRow()) {
+            for (Cell cell : ticketRow.getSetCell()) {
+                if (!cell.isCrossed()) {
+                    isWinner = false;
+                    return isWinner;
+                }
+                countCrossedNumber++;
+            }
+        }
+        if (isWinner) {
+            Tools.customLogger("Билет номер " + ticket.getNumber() + " ВЫИГРАЛ В ПОСЛЕДУЮЩЕМ ТУРЕ СОВПАДЕНИЕМ ВСЕХ ЧИСЕЛ В БИЛЕТЕ = " + countCrossedNumber);
+
+            return isWinner;
+        }
+        return isWinner;
     }
 
     public static void crossedTicket(Ticket ticket, int number) {
