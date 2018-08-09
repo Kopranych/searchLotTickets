f8ed1d62e5591ebe4aa601749720364ce2a30284
Kopranych <kopranych@gmail.com>
2018-08-10 00:25:29 +0300

    [ADD] - Start добавил проверки вводимых чисел

--- src/main/java/lottery/Start.java
+++ src/main/java/lottery/Start.java
@@ -6,17 +6,22 @@ import model.Ticket;
 import ru.bpirate.vsrftools.Tools;
 
 import java.io.File;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
 import static logic.CompareTicket.listUniqueTicket;
 import static lottery.verification.VerificationWinningTickets.*;
 
 public class Start {
+
     public static void main(String[] args) {
+        Set<Integer> setNumberOutBag = new HashSet<>();
+        List<Integer> listRangeNumber = new ArrayList<>();
+        for (int i = 0; i < 90; i++) {
+            listRangeNumber.add(i, i + 1);
+        }
+        int countEnteredNumber = 0;
         Tools.customLogger("> > Начала работу программа проверки выигрышных билетов");
-        ConvertingTicket.uploadTicket(new File("src\\main\\java\\data\\ticket\\" +  "2018-08-04T20-25-26-820").getPath());
+        ConvertingTicket.uploadTicket(new File("src\\main\\java\\data\\ticket\\" + "2018-08-04T20-25-26-820").getPath());
         for (Ticket ticket : listUniqueTicket) {
             CompareTicket.movingFromRowToCell(ticket);
         }
@@ -25,31 +30,47 @@ public class Start {
         listTicketSecondTour = new LinkedList<>(listUniqueTicket);
         listTicketNextTout = new LinkedList<>(listUniqueTicket);
 
-        while(numberOutBag != 100) {
-            Tools.customLogger("* Введите число");
+        while (numberOutBag != 100) {
+            Tools.customLogger("* Ход " + ++countEnteredNumber + " введите число от 1 до 90");
             Scanner scanner = new Scanner(System.in);
             numberOutBag = scanner.nextInt();
-            if(numberOutBag == 100){
+            if (numberOutBag == 100) {
                 Tools.customLogger("* Игра закончена");
                 break;
             }
-            Iterator<Ticket> iteratorFirstTout = listTicketFirstTour.iterator();
-            Iterator<Ticket> iteratorSecondTout = listTicketSecondTour.iterator();
-            Iterator<Ticket> iteratorNextTout = listTicketNextTout.iterator();
-            while(iteratorFirstTout.hasNext()){
-                Ticket ticket = iteratorFirstTout.next();
-                crossedTicket(ticket, numberOutBag);//вычеркиваем совпадающие цифры
-            }
-            while(iteratorSecondTout.hasNext()){
-                Ticket ticket = iteratorSecondTout.next();
-                crossedTicket(ticket, numberOutBag);//вычеркиваем совпадающие цифры
-            }
-            while(iteratorNextTout.hasNext()){
-                Ticket ticket = iteratorNextTout.next();
-                crossedTicket(ticket, numberOutBag);//вычеркиваем совпадающие цифры
+            if (setNumberOutBag.add(numberOutBag) & numberOutBag <= 90) {
+                Tools.customLogger("* Введите номер тура");
+                Scanner scan = new Scanner(System.in);
+                int numTour = scan.nextInt();
+
+                listRangeNumber.remove(new Integer(numberOutBag));
+                Iterator<Ticket> iteratorFirstTout = listTicketFirstTour.iterator();
+                Iterator<Ticket> iteratorSecondTout = listTicketSecondTour.iterator();
+                Iterator<Ticket> iteratorNextTout = listTicketNextTout.iterator();
+                while (iteratorFirstTout.hasNext()) {
+                    Ticket ticket = iteratorFirstTout.next();
+                    crossedTicket(ticket, numberOutBag);//вычеркиваем совпадающие цифры
+                }
+                while (iteratorSecondTout.hasNext()) {
+                    Ticket ticket = iteratorSecondTout.next();
+                    crossedTicket(ticket, numberOutBag);//вычеркиваем совпадающие цифры
+                }
+                while (iteratorNextTout.hasNext()) {
+                    Ticket ticket = iteratorNextTout.next();
+                    crossedTicket(ticket, numberOutBag);//вычеркиваем совпадающие цифры
+                }
+
+                verificationWinningTicket(numberOutBag, numTour);
+            } else {
+                Tools.customLogger("Такая цифра " + numberOutBag + " уже была");
+                Tools.customLogger("Попробуйте цифру из списка");
+                for (Integer i : listRangeNumber) {
+                    System.out.print(i + ", ");
+                }
             }
 
-            verificationWinningTicket(numberOutBag);
         }
+        //TODO выводить статистику по всем билетам в конце
+        //TODO проверить на невыпавшие числа
     }
 }
